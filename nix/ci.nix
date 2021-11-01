0cbaf62b43ba96cc53a4728793c3db6f6f2d53b0
gege251 <gege251@gmail.com>
2021-11-01 09:44:37 +0900

    Remove unused files

--- nix/ci.nix
+++ nix/ci.nix
@@ -7,19 +7,12 @@ let
     inherit sourcesFile system sources plutus deferPluginErrors doCoverage;
   };
   npmlock2nix = plutus.pkgs.callPackage sources.npmlock2nix { };
-  solc = import ./solc.nix {
-    inherit (plutus.pkgs)
-      lib gccStdenv fetchzip boost cmake coreutils fetchpatch ncurses python3 z3
-      cvc4 cln gmp;
-  };
 in rec {
   # What should CI build?
 
   inherit (project) projectCoverageReport;
   inherit (project.mlabs-pab.components) library;
   inherit (project.mlabs-pab.components.tests) mlabs-pab-test;
-
-  inherit solc;
 
   # This will run the tests within this build and produce the test logs as output
   check = plutus.pkgs.runCommand "run-tests" { } ''
