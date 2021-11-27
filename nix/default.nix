d1679edc376cb76b83cabde89cab87ed94f3a1e6
gege251 <gege251@gmail.com>
2021-11-27 13:52:55 +0100

    Remove unused packages from nix

--- nix/default.nix
+++ nix/default.nix
@@ -4,9 +4,5 @@
     # See: https://github.com/input-output-hk/plutus/blob/893a887eac83409131b2038820a14962c6796776/ci.nix#L5
     #rev = "fake";
   };
-  pkgs = plutus.pkgs // {
-    npmlock2nix = pkgs.callPackage sources.npmlock2nix { };
-  };
-  pab = import ./pab.nix { inherit plutus; };
   nixpkgs = import sources.nixpkgs { };
 }
