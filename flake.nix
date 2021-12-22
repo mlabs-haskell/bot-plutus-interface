6733458d34e09b9296a8ea9614b5400deee487d3
gege251 <gege251@gmail.com>
2021-12-22 09:09:29 +0100

    Simplify sha256map in flakes

--- flake.nix
+++ flake.nix
@@ -109,45 +109,12 @@
               ps.word-array
             ];
           };
-          sha256map = {
-            # i.e. this will now fail to nix-build if you bump it but don't bump the `cabal.project`.
-            "https://github.com/input-output-hk/plutus"."${sources.plutus.rev}" =
-              sources.plutus.sha256;
-            "https://github.com/input-output-hk/plutus-apps.git"."${sources.plutus-apps.rev}" =
-              sources.plutus-apps.sha256;
-            "https://github.com/Quid2/flat.git"."${sources.flat.rev}" =
-              sources.flat.sha256;
-            "https://github.com/input-output-hk/purescript-bridge.git"."${sources.purescript-bridge.rev}" =
-              sources.purescript-bridge.sha256;
-            "https://github.com/input-output-hk/servant-purescript.git"."${sources.servant-purescript.rev}" =
-              sources.servant-purescript.sha256;
-            "https://github.com/input-output-hk/cardano-base"."${sources.cardano-base.rev}" =
-              sources.cardano-base.sha256;
-            "https://github.com/input-output-hk/cardano-crypto.git"."${sources.cardano-crypto.rev}" =
-              sources.cardano-crypto.sha256;
-            "https://github.com/input-output-hk/cardano-ledger-specs"."${sources.cardano-ledger-specs.rev}" =
-              sources.cardano-ledger-specs.sha256;
-            "https://github.com/input-output-hk/cardano-prelude"."${sources.cardano-prelude.rev}" =
-              sources.cardano-prelude.sha256;
-            "https://github.com/input-output-hk/goblins"."${sources.goblins.rev}" =
-              sources.goblins.sha256;
-            "https://github.com/input-output-hk/iohk-monitoring-framework"."${sources.iohk-monitoring-framework.rev}" =
-              sources.iohk-monitoring-framework.sha256;
-            "https://github.com/input-output-hk/ouroboros-network"."${sources.ouroboros-network.rev}" =
-              sources.ouroboros-network.sha256;
-            "https://github.com/input-output-hk/cardano-node.git"."${sources.cardano-node.rev}" =
-              sources.cardano-node.sha256;
-            "https://github.com/input-output-hk/Win32-network"."${sources.Win32-network.rev}" =
-              sources.Win32-network.sha256;
-            "https://github.com/input-output-hk/hedgehog-extras"."${sources.hedgehog-extras.rev}" =
-              sources.hedgehog-extras.sha256;
-            "https://github.com/input-output-hk/optparse-applicative"."${sources.optparse-applicative.rev}" =
-              sources.optparse-applicative.sha256;
-            "https://github.com/input-output-hk/cardano-addresses"."${sources.cardano-addresses.rev}" =
-              sources.cardano-addresses.sha256;
-            "https://github.com/j-mueller/cardano-wallet"."${sources.cardano-wallet.rev}" =
-              sources.cardano-wallet.sha256;
-          };
+          sha256map = pkgs.lib.foldr (data: tab:
+            with data;
+            tab // {
+              "https://github.com/${owner}/${repo}"."${rev}" = sha256;
+              "https://github.com/${owner}/${repo}.git"."${rev}" = sha256;
+            }) { } (pkgs.lib.attrValues sources);
         };
     in {
       project = perSystem projectFor;
