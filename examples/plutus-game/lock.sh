d7ea63617cccae8e16acd8021c0495e6cd3d119c
gege251 <gege251@gmail.com>
2022-01-04 22:59:11 +0100

    Add guessing game example to test datums and redeemers

---
+++ examples/plutus-game/lock.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CONTRACT_INST_ID=$(curl --location --request POST 'localhost:9080/api/contract/activate' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+    "caID": {
+        "tag": "Lock",
+        "contents": {
+            "lockGameId": 2,
+            "lockAmount": 1000000,
+            "lockSecret": "secret"
+        }
+    }
+  }' | jq -r .unContractInstanceId )
+
+echo $CONTRACT_INST_ID
+
+
+echo "{ \"tag\": \"Subscribe\", \"contents\": { \"Left\": { \"unContractInstanceId\":\"$CONTRACT_INST_ID\" } } }" | websocat -n ws://localhost:9080/ws 
+
