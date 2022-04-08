d7d8f85efe649dc628ca61ca2e3257f8f46b007d
Aleksandr Penskoi <aleksandr@mlabs.city>
2022-04-08 03:18:06 +0300

    Update README.md and examples.

--- examples/plutus-transfer/app/Main.hs
+++ examples/plutus-transfer/app/Main.hs
@@ -4,28 +4,20 @@
 module Main (main) where
 
 import BotPlutusInterface qualified
+import BotPlutusInterface.Config qualified as BotPlutusInterface
 import BotPlutusInterface.Types (
-  CLILocation (Local),
   HasDefinitions (..),
-  LogLevel (Debug),
-  PABConfig (..),
   SomeBuiltin (..),
   endpointsToSchemas,
  )
-import Cardano.Api (NetworkId (Testnet), NetworkMagic (..))
 import Cardano.PlutusExample.Transfer (
   TransferParams,
   TransferSchema,
   transfer,
  )
-import Data.Aeson qualified as JSON
 import Data.Aeson.TH (defaultOptions, deriveJSON)
-import Data.ByteString.Lazy qualified as LazyByteString
-import Data.Default (def)
-import Data.Maybe (fromMaybe)
 import Playground.Types (FunctionSchema)
 import Schema (FormSchema)
-import Servant.Client.Core (BaseUrl (BaseUrl), Scheme (Http))
 import Prelude
 
 instance HasDefinitions TransferContracts where
@@ -47,27 +39,7 @@ $(deriveJSON defaultOptions ''TransferContracts)
 
 main :: IO ()
 main = do
-  protocolParams <-
-    fromMaybe (error "protocol.json file not found") . JSON.decode
-      <$> LazyByteString.readFile "protocol.json"
-  let pabConf =
-        PABConfig
-          { pcCliLocation = Local
-          , pcNetwork = Testnet (NetworkMagic 1097911063)
-          , pcChainIndexUrl = BaseUrl Http "localhost" 9083 ""
-          , pcPort = 9080
-          , pcProtocolParams = protocolParams
-          , pcTipPollingInterval = 10_000_000
-          , pcSlotConfig = def
-          , pcOwnPubKeyHash = "0f45aaf1b2959db6e5ff94dbb1f823bf257680c3c723ac2d49f97546"
-          , pcOwnStakePubKeyHash = Nothing
-          , pcScriptFileDir = "./scripts"
-          , pcSigningKeyFileDir = "./signing-keys"
-          , pcTxFileDir = "./txs"
-          , pcDryRun = True
-          , pcLogLevel = Debug
-          , pcProtocolParamsFile = "./protocol.json"
-          , pcForceBudget = Nothing
-          , pcEnableTxEndpoint = True
-          }
+  pabConf <-
+    either error id
+      <$> BotPlutusInterface.loadPABConfig "config/pabConfig.value"
   BotPlutusInterface.runPAB @TransferContracts pabConf
