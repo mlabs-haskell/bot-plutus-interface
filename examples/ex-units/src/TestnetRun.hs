b56363e088d524bbe6322165dd8edf0c968f76b4
Mikhail Lazarev <mikhail@mlabs.city>
2022-05-26 12:20:16 +0300

    wip: slot <-> time convcersion - testnet debugging enhancements

--- examples/ex-units/src/TestnetRun.hs
+++ examples/ex-units/src/TestnetRun.hs
@@ -10,35 +10,41 @@ import Data.Aeson qualified as JSON
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.UUID.V4 qualified as UUID
-import Ledger (PubKeyHash)
-import TimeDebugContract qualified
-
 import GHC.IO.Encoding
+import Ledger (PubKeyHash)
 import Plutus.PAB.Core.ContractInstance.STM (Activity (Active))
 import Servant.Client (BaseUrl (BaseUrl), Scheme (Http))
 import System.Directory (listDirectory)
-import System.Environment (getEnv, setEnv)
+import System.Environment (getArgs, getEnv, setEnv)
 import System.FilePath ((</>))
+import TimeDebugContract qualified
 import Wallet.Types (ContractInstanceId (ContractInstanceId))
 import Prelude
 
 testnetRun :: IO ()
 testnetRun = do
   setLocaleEncoding utf8
-  -- [sockPath, clusterDir, cliDir] <- getArgs
-  let sockPath = "/home/mike/dev/mlabs/testnet-bpi-setup/socket/node.socket"
-      cliDir = "/home/mike/dev/iog/binaries"
-      bpiDir = "/home/mike/dev/mlabs/testnet-bpi-setup/data"
+  [bpiDir, cliDir, sockPath, operation] <- getArgs -- /home/mike/dev/mlabs/net-setups/testnet-bpi-setup/data
   setEnv "CARDANO_NODE_SOCKET_PATH" sockPath
   getEnv "PATH" >>= \p -> setEnv "PATH" (p ++ ":" ++ cliDir)
 
   cEnv <- mkContractEnv bpiDir
 
   putStrLn "Running contract"
-  -- res <- BPI.runContract cEnv TimeDebugContract.timeDebugLight
-  -- res <- BPI.runContract cEnv TimeDebugContract.splitUtxo
-  -- res <- BPI.runContract cEnv TimeDebugContract.lockAtScript
-  res <- BPI.runContract cEnv TimeDebugContract.unlockWithTimeCheck
+  res <- case operation of
+    "light" -> do
+      putStrLn "Running loght debug"
+      BPI.runContract cEnv TimeDebugContract.timeDebugLight
+    "split" -> do
+      putStrLn "Splitting whatever first utxo"
+      fmap show <$> BPI.runContract cEnv TimeDebugContract.splitUtxo
+    "lock" -> do
+      putStrLn "Locking"
+      BPI.runContract cEnv TimeDebugContract.lockAtScript
+    "unlock" -> do
+      putStrLn "Spending"
+      BPI.runContract cEnv TimeDebugContract.unlockWithTimeCheck
+    other -> error $ "Unsupported operation: " ++ other
 
   putStrLn $ case res of
     Right r -> "=== OK ===\n" ++ show r
@@ -57,32 +63,32 @@ mkContractEnv bpiDir = do
   pkhs <- getPkhs bpiDir
   return $
     ContractEnvironment
-      { cePABConfig = mkPabConf pparams (Text.pack paramsFile) bpiDir (head pkhs)
-      , ceContractState = contractState
-      , ceContractInstanceId = contractInstanceID
-      , ceContractStats = contractStats
+      { cePABConfig = mkPabConf pparams (Text.pack paramsFile) bpiDir (head pkhs),
+        ceContractState = contractState,
+        ceContractInstanceId = contractInstanceID,
+        ceContractStats = contractStats
       }
 
 mkPabConf :: ProtocolParameters -> Text -> FilePath -> PubKeyHash -> PABConfig
 mkPabConf pparams pparamsFile bpiDir ownPkh =
   PABConfig
-    { pcCliLocation = Local
-    , pcNetwork = Testnet (NetworkMagic 1097911063)
-    , pcChainIndexUrl = BaseUrl Http "localhost" 9083 ""
-    , pcPort = 9080
-    , pcProtocolParams = pparams
-    , pcTipPollingInterval = 1_000_000
-    , pcOwnPubKeyHash = ownPkh
-    , pcOwnStakePubKeyHash = Nothing
-    , pcScriptFileDir = Text.pack $ bpiDir </> "scripts"
-    , pcSigningKeyFileDir = Text.pack $ bpiDir </> "signing-keys"
-    , pcTxFileDir = Text.pack $ bpiDir </> "txs"
-    , pcDryRun = False
-    , pcLogLevel = Error
-    , pcProtocolParamsFile = pparamsFile
-    , pcEnableTxEndpoint = True
-    , pcCollectStats = True
-    , pcMetadataDir = Text.pack $ bpiDir </> "metadata"
+    { pcCliLocation = Local,
+      pcNetwork = Testnet (NetworkMagic 1097911063),
+      pcChainIndexUrl = BaseUrl Http "localhost" 9083 "",
+      pcPort = 9080,
+      pcProtocolParams = pparams,
+      pcTipPollingInterval = 1_000_000,
+      pcOwnPubKeyHash = ownPkh,
+      pcOwnStakePubKeyHash = Nothing,
+      pcScriptFileDir = Text.pack $ bpiDir </> "scripts",
+      pcSigningKeyFileDir = Text.pack $ bpiDir </> "signing-keys",
+      pcTxFileDir = Text.pack $ bpiDir </> "txs",
+      pcDryRun = False,
+      pcLogLevel = Error,
+      pcProtocolParamsFile = pparamsFile,
+      pcEnableTxEndpoint = True,
+      pcCollectStats = True,
+      pcMetadataDir = Text.pack $ bpiDir </> "metadata"
     }
 
 getPkhs :: FilePath -> IO [PubKeyHash]
